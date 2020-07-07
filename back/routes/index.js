1eba4eeb9467876aa0cd7223b3c9faa13a5e7d7f
pyrce <fetheve2@gmail.com>
2020-07-07 10:31:56 +0400

    ajout de routes pour les vues

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var indexController=require('../controllers/voteController.js');
+var indexController=require('../controllers/indexController.js');
 var usersController=require('../controllers/usersController.js');
 /* GET home page. */
 
-router.get('/',indexController.liste );
-router.get('/api/ajout',indexController.ajout );
-router.get('/api/votes/:id',indexController.detailvote );
-router.post('/api/votes',indexController.addvote );
-router.delete('/api/votes/:id',indexController.deletevote );
-router.put('/api/votes/:id',indexController.updatevote );
+router.get('/',indexController.list );
+router.get('/ajout',indexController.ajout );
+router.get('/votes/:id',indexController.show );
+router.post('/votes',indexController.add );
+router.delete('/votes/:id',indexController.delete );
+router.put('/votes/:id',indexController.update );
 
 router.post("/api/login",usersController.login);
 router.post("/api/login",usersController.signup);
