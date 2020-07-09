d1de299db88c87fc44d588b499c0fca3d63bdc67
Rabehoavy <rabehoavy@gmail.com>
2020-07-09 12:02:29 +0400

    Seeds collection

--- back/migrations/migrations/20200709055018-create_collection_users_votes.js
+++ back/migrations/migrations/20200709055018-create_collection_users_votes.js
@@ -4,11 +4,11 @@ module.exports = {
     // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
     // Example:
     // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
-    return await db.createCollection('users_votes', {
+    return await db.createCollection('usersVotes', {
       validator: {
         $jsonSchema: {
           bsonType: "object",
-          required: ["user", "vote", "choice"],
+          required: ["user", "vote", "choix"],
           properties: {
             user: {
               bsonType: 'objectId'
@@ -31,6 +31,6 @@ module.exports = {
     // TODO write the statements to rollback your migration (if possible)
     // Example:
     // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
-    return await db.collection('users_votes').drop()
+    return await db.collection('usersVotes').drop()
   }
 };
